ff286bb14c741ee0bc785d7dcbe4d291677c4a42
John-gr <20243ds060@utez.edu.mx>
2025-10-17 14:16:13 -0600

    Agregado localstorage en app, estilos para las secciones de css y correciones en index por semantica

--- js/app.js
+++ js/app.js
@@ -1,14 +1,47 @@
 const btnAceptar=document.querySelector(".btnAceptar");
 const name=document.querySelector(".nombre");
 const age=document.querySelector(".edad");
-const email=document.querySelector(".email");
+const mail=document.querySelector(".email");
 const paises=document.querySelector(".paises");
 const mensaje=document.querySelector(".mensaje");
+const storage=document.querySelector(".storage");
+
+const nombreLS=localStorage.getItem("nombre");
+const edadLS=localStorage.getItem("edad");
+const emailLS=localStorage.getItem("email");
+const paisLS=localStorage.getItem("pais");
+if(nombreLS!=""&&edadLS!=""&&emailLS!=""&&paisLS!=""){
+    storage.textContent=`Hola ${nombreLS}, edad:${edadLS}, email:${emailLS}, pais:${paisLS}`
+}
 
 function aceptar(){
     const nombre=name.value
+    const edad=age.value;
+    const email=mail.value;
+    const pais=paises.value;
     if(nombre===""){
-        mensaje.innerHTML="<p>Error</p>"
+        mensaje.innerHTML="<p>Error falta nombre</p>"
+    } else{
+        localStorage.setItem("nombre",nombre);
+        mensaje.textContent="";
+    }
+
+    if(edad===""){
+        mensaje.innerHTML="<p>Error falta edad</p>"
+    } else{
+        localStorage.setItem("edad",edad);
+    }
+    
+    if(email===""){
+        mensaje.innerHTML="<p>Error falta email</p>"
+    } else{
+        localStorage.setItem("email",email);
+    }
+
+    if(pais===""){
+        mensaje.innerHTML="<p>Error falta pais</p>"
+    } else{
+        localStorage.setItem("pais",pais);
     }
 }
 
